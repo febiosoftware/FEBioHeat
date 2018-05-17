11776e6662128f0a31bb9d34f95a070055ae2693
SteveMaas1978 <steve.maas@utah.edu>
2018-05-17 16:28:40 +0000

    FEBioHeat: First commit of the FEBioHeat plugin.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Plugins/FEBioHeat@10892 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBioHeat/FEBioHeat.cpp
@@ -0,0 +1,37 @@
+#include <FECore/sdk.h>
+#include "FEIsotropicFourier.h"
+#include "FEPlotHeatFlux.h"
+#include "FEHeatFlux.h"
+#include "FEConvectiveHeatFlux.h"
+#include "FEHeatSolver.h"
+#include "FEHeatSolidDomain.h"
+#include "FEHeatDomainFactory.h"
+#include "FEHeatSource.h"
+#include "FEHeatSolidDomain.h"
+
+FECORE_PLUGIN void PluginInitialize(FECoreKernel& fecore)
+{
+	FECoreKernel::SetInstance(&fecore);
+
+	// create the heat-transfer module
+	fecore.CreateModule("heat");
+
+	// Domain factory
+	fecore.RegisterDomain(new FEHeatDomainFactory);
+	REGISTER_FECORE_CLASS(FEHeatSolidDomain, FEDOMAIN_ID, "heat-solid");
+
+	// feature classes
+	REGISTER_FECORE_CLASS(FEHeatSolver        , FESOLVER_ID     , "heat");
+	REGISTER_FECORE_CLASS(FEIsotropicFourier  , FEMATERIAL_ID   , "isotropic Fourier");
+	REGISTER_FECORE_CLASS(FEConstHeatSource   , FEBODYLOAD_ID   , "heat_source");
+	REGISTER_FECORE_CLASS(FEHeatFlux          , FESURFACELOAD_ID, "heatflux");
+	REGISTER_FECORE_CLASS(FEConvectiveHeatFlux, FESURFACELOAD_ID, "convective_heatflux");
+	REGISTER_FECORE_CLASS(FEPlotHeatFlux      , FEPLOTDATA_ID   , "heat flux");
+
+	fecore.SetActiveModule(0);
+}
+
+FECORE_PLUGIN int GetSDKVersion()
+{
+	return FE_SDK_VERSION;
+}
