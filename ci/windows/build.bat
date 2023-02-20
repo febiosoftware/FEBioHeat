e76eb1001e4fc4c547a7f52a713fdbdb9fde3f78
John Haynes <tundranerd@gmail.com>
2023-02-20 16:36:13 -0700

    WIP Windows CI

---
+++ ci/windows/build.bat
@@ -0,0 +1,11 @@
+call "%ONEAPI_ROOT%\setvars.bat"
+set /a "PROC=%NUMBER_OF_PROCESSORS%"
+cmake -version
+
+cmake -L . -B cmbuild ^
+  -DFEBio_SDK=febio-sdk
+)
+cd cmbuild
+msbuild /P:Configuration=Release /P:WarningLevel=0 /m:%PROC% ALL_BUILD.vcxproj
+cd ..
+exit 0
