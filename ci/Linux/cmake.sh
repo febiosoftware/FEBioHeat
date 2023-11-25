acc272bcd3078e68be7b14b1f9b4b6e63a591119
John Haynes <tundranerd@gmail.com>
2023-11-25 09:32:33 -0700

    Linux reusable workflow

--- ci/Linux/cmake.sh
+++ ci/Linux/cmake.sh
@@ -1,5 +1,5 @@
 LIB_DIR="/usr/lib/x86_64-linux-gnu"
-FEBIO_SDK="${FEBIO_SDK:-/febio-sdk}"
+FEBIO_SDK="${FEBIO_SDK:-febio4-sdk}"
 run_cmake() {
 	cmake -L . -B cmbuild \
 		-DFEBio_SDK=$FEBIO_SDK
---
+++ ci/Linux/scp-to-repo.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+scp cmbuild/lib/* repo:~/update2/FEBioStudio2Dev/Linux/stage/lib
