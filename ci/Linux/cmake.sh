a59f41e00a8ceea53d73520dcd328bf9039b67df
michaelrossherron <michaelrossherron@gmail.com>
2024-05-09 16:59:35 -0600

    First pass at adding plugins to FEBio push workflow

--- ci/Linux/cmake.sh
+++ ci/Linux/cmake.sh
@@ -1,5 +1,11 @@
 LIB_DIR="/usr/lib/x86_64-linux-gnu"
-FEBIO_SDK="${FEBIO_SDK:-febio4-sdk}"
+
+if [ -d febio4-sdk ]; then
+    FEBIO_SDK="${FEBIO_SDK:-febio4-sdk}"
+else
+    FEBIO_SDK="${FEBIO_SDK:-..}"
+fi
+
 run_cmake() {
 	cmake -L . -B cmbuild \
 		-DFEBio_SDK=$FEBIO_SDK
